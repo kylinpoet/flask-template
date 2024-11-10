2cc23546cb09d5e27bb10177e874ff5630448825
kylinpoet <kylinpoet@gmail.com>
2024-11-10 11:15:10 +0000

    add time tips to renew info

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def index():
     return "Welcome, this is a Flask app deployed on Zeabur"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
+    app.run(debug=True, port=os.getenv("PORT", default=5010), host='0.0.0.0')
